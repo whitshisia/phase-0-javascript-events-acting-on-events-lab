6f510739ae141007020b1bf63c908c56e3db4010
whitney shisia <shisiawhitney215@gmail.com>
2024-03-20 07:32:49 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+//   document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowLeft") {
+//       moveDodgerLeft();
+//     }
+//   });
+  
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    let newPosition = left + 40;
+  
+    if (newPosition > 360) {
+      newPosition = 360;
+    }
+        dodger.style.left = `${newPosition}px`;
+      }
